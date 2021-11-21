a673854f61899d1d82facf4454e9dbbcb21caaff
John Leavell <johnleavell@gmail.com>
2021-11-20 22:22:36 -0600

    crud functionality for locations

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 const { Router } = require("express");
+const locationController = require("../controllers/location.js");
 
 const router = Router();
+
+//location endpoints
+router.get("/api/locations/:id", locationController.getLocationById);
+router.post("/api/location", locationController.create);
+router.put("/api/locations/:id", locationController.update);
+router.delete("/api/locations/:id", locationController.deleteLocation);
+router.get("/api/locations", locationController.findAll);
 
 module.exports = router;
